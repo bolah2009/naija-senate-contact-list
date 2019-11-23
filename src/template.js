04be1fb773b830b1509bbf4d80b518089a3b95e5
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-11-23 12:09:31 +0100

    Add template

---
+++ src/template.js
@@ -0,0 +1,13 @@
+export const subject = "The Anti-Social Media Bill";
+
+export const body = encodeURIComponent(`Good day Sir/Ma,
+
+My name is ____, a concerned constituent
+from ____. I write this mail to strongly condemn the anti-social media bill (hereinafter "the bill") currently in the committee stage at the Red chambers and to implore you to vote against this bill.
+
+The bill seeks to gag Nigerians constitutional right to freedom of expression and speech under the guise of combating false information and "fake news". It is an attempt at regulating the citizens right to demand accountability and transparency from its elected officials and government officials and agencies.
+
+I urge you Sir in your capacity to vote against this bill and also speak against this bill in further plenary sessions. The right to speech and freedom of expression is an essential feature of democracy and when this is threatened, our democracy is threatened.
+
+Best regards,
+*Name*`);
