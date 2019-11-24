0c9ed69035784cb6e295ece548b340bc677d117a
Victor Bagu <victorbagu@gmail.com>
2019-11-24 15:02:29 +0200

    Small grammar fix

--- src/template.js
+++ src/template.js
@@ -8,7 +8,7 @@ from ${district}. I write this mail to strongly condemn the anti-social media bi
 
 The bill seeks to gag Nigerians constitutional right to freedom of expression and speech under the guise of combating false information and "fake news". It is an attempt at regulating the citizens right to demand accountability and transparency from its elected officials and government officials and agencies.
 
-I urge you Sir in your capacity to vote against this bill and also speak against this bill in further plenary sessions. The right to speech and freedom of expression is an essential feature of democracy and when this is threatened, our democracy is threatened.
+I urge you Sir/Ma in your capacity to vote against this bill and also speak against it in further plenary sessions. The right to speech and freedom of expression is an essential feature of democracy and when this is threatened, our democracy is also threatened.
 
 Best regards,
 *Name*`);
