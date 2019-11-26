803cb2cc0b34be9f7ff1270a402aae614cda09eb
Victor Bagu <victorbagu@gmail.com>
2019-11-26 02:04:11 +0200

    Precise communication type in message body

--- src/template.js
+++ src/template.js
@@ -1,9 +1,9 @@
 export const subject = 'The Anti-Social Media Bill';
 
-export const body = district =>
+export const body = (district, commType) =>
   encodeURIComponent(`Good day Sir/Ma,
 
-My name is ____, a concerned constituent from ${district}. I write this mail to strongly condemn The Anti-Social Media Bill (hereinafter "the bill") currently in the committee stage at the Red chambers and to implore you to vote against this bill.
+My name is ____, a concerned constituent from ${district}. I write this ${commType} to strongly condemn The Anti-Social Media Bill (hereinafter "the bill") currently in the committee stage at the Red chambers and to implore you to vote against this bill.
 
 The bill seeks to gag Nigerians' constitutional right to freedom of expression and speech under the guise of combating false information and "fake news". It is an attempt at regulating the citizens' right to demand accountability and transparency from its elected officials and government officials and agencies.
 
